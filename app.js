c4fe9569d2a36f5937227f83fdf2e9ae6e8c5d9b
Conchetta Park <conchettapark@MacBook-Pro.local>
2020-08-02 18:14:43 -0400

    final changes

--- app.js
+++ app.js
@@ -8,11 +8,117 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const employeeTypeMap = {
+  Manager,
+  Engineer,
+  Intern,
+};
 
+const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+var managerQuestions = [
+  {
+    name: "name",
+    type: "input",
+    message: "What is your manager's name?",
+  },
+
+  {
+    name: "id",
+    type: "input",
+    message: "What is your manager's id?",
+  },
+
+  {
+    name: "email",
+    type: "input",
+    message: "What is your manager's email?",
+  },
+
+  {
+    name: "officeNumber",
+    type: "input",
+    message: "What is your manager's office number?",
+  },
+];
+var employeeQuestions = [
+  {
+    name: "TeamMemberType",
+    type: "list",
+    message: "Which type of team member would you like to add?",
+    choices: ["Engineer", "Intern", "I do not want to add a team member"],
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "What is your engineer's name?",
+    when: (answers) => answers.TeamMemberType == "Engineer",
+  },
+  {
+    name: "id",
+    type: "input",
+    message: "What is your engineer's id?",
+    when: (answers) => answers.TeamMemberType == "Engineer",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is your engineer's email?",
+    when: (answers) => answers.TeamMemberType == "Engineer",
+  },
+  {
+    name: "github",
+    type: "input",
+    message: "What is your engineer's github username?",
+    when: (answers) => answers.TeamMemberType == "Engineer",
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "What is your intern's name?",
+    when: (answers) => answers.TeamMemberType == "Intern",
+  },
+  {
+    name: "id",
+    type: "input",
+    message: "What is your intern's id?",
+    when: (answers) => answers.TeamMemberType == "Intern",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is your intern's email?",
+    when: (answers) => answers.TeamMemberType == "Intern",
+  },
+  {
+    name: "school",
+    type: "input",
+    message: "What is your intern's school?",
+    when: (answers) => answers.TeamMemberType == "Intern",
+  },
+];
+var employees = [];
+
+inquirer.prompt(managerQuestions).then((managerAnswers) => {
+  inquirer.prompt(employeeQuestions).then((employeeAnswers) => {
+    const m = new Manager(managerAnswers);
+    employees.push(m);
+
+    if (employeeTypeMap[employeeAnswers.TeamMemberType]) {
+      const e = new employeeTypeMap[employeeAnswers.TeamMemberType](
+        employeeAnswers
+      );
+      employees.push(e);
+    }
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, render(employees));
+  });
+});
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
